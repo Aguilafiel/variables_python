6d01b74a2c4de248abd2115fd67f4b210e06abba
Aguilafiel <111097551+Aguilafiel@users.noreply.github.com>
2022-08-31 15:15:31 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -1,4 +1,4 @@
-# Condicionales [Python]
+# Tipos de variables [Python]
 # Ejercicios de práctica
 
 # Autor: Inove Coding School
@@ -7,52 +7,40 @@
 # IMPORTANTE: NO borrar los comentarios
 # que aparecen en verde con el hashtag "#"
 
-# Ejemplos variables de texto
+# Ejercicios de práctica numérica y consola
 
-# Comparadores
-# Ingrese dos palabras cualesquiera y realice las sigueintes
-# comparaciones entre ellas
-texto_1 = str(input('Ingrese la primera palabra:\n'))
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+from __future__ import division
 
-texto_2 = str(input('Ingrese la segunda palabra:\n'))
 
-# Compare cual de las dos palabras es mayor (alfabéticamente)
-# Imprima en pantalla según corresponda
-if texto_1 < texto_2:
-    print(texto_2 , 'es mayor que' , texto_1)
-else:
-    print(texto_1 , 'es mayor que' , texto_2)
+print('Ingrese por consola el primer número entero a operar:')
+numero_1 = int(input())
 
-# Compare cual de las dos palabras tiene mayor
-# cantidad de letras
-# Imprima en pantalla según corresponda
+print('Ingrese por consola el segundo número entero a operar:')
+numero_2 = int(input())
+print('los numeros ingresados son', numero_1 ,'y' , numero_2) 
+suma = numero_1 + numero_2 
+resta = numero_1 - numero_2 
+mult = numero_1 * numero_2
+div = numero_1 / numero_2
+print('resultado de la suma',numero_1 ,'y' , numero_2 ,'es', suma)
+print('resultado de la resta de resta' , numero_1 , 'y' , numero_2 ,'es' , resta)
+print('resultado de la multiplicacion' , numero_1, 'y' , numero_2 ,'es' , mult)
+print('resultado de la  division' , numero_1 , 'y' , numero_2 , 'es' , div)
+# Alumno: Imprima en pantalla los dos números enteros solicitados
+# print(....)
 
-if len(texto_1) < len(texto_2):
-    print(texto_2 , 'es mas larga que' , texto_1)
-elif len(texto_2) < len(texto_1):
-    print(texto_1 , 'es mas largo que' , texto_2)
-else:
-    print( texto_2 , 'y' , texto_1 , 'miden lo mismo' )
-    
+# Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
+# numero_1, numero_2
+# Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
+# El resultado de sumar 4 y 2 es 6
+# NOTA: No coloque usted los nùmeros y resultados, use las variables
 
-# Verifique si la primera letra de la primera palabra
-# es mayor a la primera letra de la segunda palabra
-# Imprima en pantalla según corresponda
-if texto_1[0] > texto_2[0]:
-    print(texto_1[0] , 'es mayor que' , texto_2[0])
-else:
-    print(print(texto_2[0] , 'es mayor que' , texto_1[0]))
+# Suma
 
-copia_texto_1 = texto_1 
-print(copia_texto_1)
- # Copia de la variable texto_1
-# Verifique que copia_texto_1 es igual a texto_1
-# Imprima en pantalla según corresponda
+# Resta
 
-# Verifique que copia_texto_1 es distinta a texto_2
-# Imprima en pantalla según corresponda
-if copia_texto_1 == texto_2:
-    print('las palabras son iguales')
-else:
-    print('las palabras son distintas')
-    
+# División
+
+# Multiplicación
--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -1,4 +1,4 @@
-# Condicionales [Python]
+# Tipos de variables [Python]
 # Ejercicios de práctica
 
 # Autor: Inove Coding School
@@ -7,56 +7,26 @@
 # IMPORTANTE: NO borrar los comentarios
 # que aparecen en verde con el hashtag "#"
 
-# Ejercicios de práctica numérica
+# Ejemplos variables de texto
 
-# Condicionales anidados
-print('ingrese primer numero')
-numero_1 = int(input())
-print('ingrese el segundon numero')
-numero_2 = int(input())
+# Ingrese primero su nombre y luego su apellido
+# Capture ambos datos e imprima su nombre completo
+print('Ingrese por consola su nombre/s:')
+nombre = str(input())
 
-# Verifique si el numero_1 es mayor a 5
-#   --> En caso afirmativo, verifique si el numero_2
-#       es positivo
-#       --> En caso afirmativo imprima en pantalla "Resp=1"
-#       --> En caso negativo imprima en pantalla   "Resp=2"
-#  --> En caso negativo (numero_1 no es mayor a 5)
-#      verifique si el numero_2 es mayor a 5
-#       --> En caso afirmativo imprima en pantalla "Resp=3"
-#       --> En caso negativo imprima en pantalla "Resp=4"
-if numero_1 > 5:
-    if  numero_2 > 0:
-        print('resp=1')
-    else:
-        print('resp=2')
-else:
-    if numero_2 > 5:
-        print('resp=3')
-    else:
-        print('resp=4')
+print('Ingrese por consola su apellido/s:')
+apellido = str(input())
+nombre_completo = nombre + ' '+ apellido
+print('su nombre completo es:' , nombre_completo)
+nc_len= len(nombre + apellido)
+print('cantidad de letras del nombre completo:', nc_len)
 
-    
-# Verifique la calificación de un estudiante según su
-# puntaje en un examen
-puntaje = 70
 
-# Si el puntaje es mayor igual a 90 --> imprimir A
-# Si el puntaje es mayor igual a 80 --> imprimir B
-# Si el puntaje es mayor igual a 70 --> imprimir C
-# Si el puntaje es mayor igual a 60 --> imprimir D
-# Si el puntaje es menor a  60      --> imprimir F
+# Imprima su nombre completo
 
-# Debe imprimir en pantalla la calificacion
-# Utilizar "if" anidados
-print('su puntaje es:' , puntaje)
-if puntaje >= 90:
-    print('su calficacion es: A ')
-elif puntaje >= 80 and puntaje < 90: 
-    print('su calficacion es: B ')
-elif puntaje >= 70 and puntaje < 80:
-    print('su calficacion es: C ')
-elif puntaje >= 60 and puntaje < 70:
-    print('su calficacion es: D ')
-else:
-    print('su calficacion es: F ')
+# Almacenar su nombre completo en una variable
+# nombre_completo = .....
 
+
+# Imprimir la cantidad de letras que posee su nombre completo
+# cantidad_letras = len(....)
